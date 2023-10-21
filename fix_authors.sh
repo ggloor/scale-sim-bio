60a32a51634dfcf54425f31f2c705e45fd5b33d3
ggloor <ggloor@uwo.ca>
2023-10-21 15:37:01 -0400

    submitted version

--- fix_authors.sh
+++ fix_authors.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
-awk '{sub(/true \\and true \\and true/,"Gregory B. Gloor$^1$ \\and Michelle Pistner Nixon$^2$ \\and Justin Silverman$^{2,3}$")};1' go3.tex > jnk.tex
+awk '{sub(/true \\and true \\and true/,"Gregory B. Gloor$^1$ \\and Michelle Pistner Nixon$^2$ \\and Justin D. Silverman$^{2,3,4}$")};1' go3.tex > jnk.tex
 
 awk '{sub(/\\date{}/,"\\date{\\today}")};1' jnk.tex > jnk2.tex
 
-awk '{sub(/\\maketitle/,"\\maketitle\n\n\$^1\$ Department of Biochemistry, University of Western Ontario\n\$^2\$ College of Information Sciences and Technology, Pennsylvania  State University\n\$^3\$ Department of Medicine, Pennsylvania  State University\n")};1' jnk2.tex > go3-author.tex
+awk '{sub(/\\maketitle/,"\\maketitle\n\n\$^1\$ Department of Biochemistry, University of Western Ontario\n\$^2\$ College of Information Sciences and Technology, Pennsylvania  State University\n\$^3\$ Department of Medicine, Pennsylvania  State University\n\$^4\$ Institute for Computational and Data Science, Pennsylvania State University\n")};1' jnk2.tex > go3-author.tex
 
 rm jnk.tex
 rm jnk2.tex
