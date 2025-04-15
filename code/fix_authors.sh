0855feb64746cb3362e4787734e222eab6ded76d
ggloor <ggloor@uwo.ca>
2025-04-15 15:40:48 -0400

    cleaned up version

--- code/fix_authors.sh
+++ code/fix_authors.sh
@@ -4,7 +4,7 @@ awk '{sub(/true \\and true \\and true/,"Gregory B. Gloor$^1$ \\and Michelle Pist
 
 awk '{sub(/\\date{}/,"\\date{\\today}")};1' jnk.tex > jnk2.tex
 
-awk '{sub(/\\maketitle/,"\\maketitle\n\n\$^1\$Department of Biochemistry, University of Western Ontario\n\$^2\$Department of Population Health Sciences, Geisinger, Danville, PA\n\$^3\$Department of Medicine, Pennsylvania State University\n\$^4\$Institute for Computational and Data Science, Pennsylvania State University\n\$^5\$Department of Statistics, Pennsylvania State University\n")};1' jnk2.tex > go3-author.tex
+awk '{sub(/\\maketitle/,"\\maketitle\n\n\$^1\$Department of Biochemistry, University of Western Ontario\n\$^2\$Department of Population Health Sciences, Geisinger, Danville, PA\n\$^3\$Department of Medicine, Pennsylvania State University\n\$^4\$Institute for Computational and Data Science, Pennsylvania State University\n\$^5\$Department of Statistics, Pennsylvania State University\n\n\$^1\$corresponding author: ggloor@uwo.ca")};1' jnk2.tex > go3-author.tex
 
 pdflatex go3-author.tex -o go3-author.pdf
 bibtex go3-author
